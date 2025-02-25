ca20a82d5a46f36505774e61550907fc3fb52af4
Divyom Chaudhary <divyomchaudhary@gmail.com>
2025-02-25 23:00:36 +0530

    Authentication and rendering

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 import express from "express";
 import bodyParser from "body-parser";
+import multer from "multer";
 
 const app= express();
-const port= 3000;
+const port= process.env.PORT || 3000;
+const storage = multer.diskStorage({
+    destination : function (req, file, cb){
+        return cb(null, "./public/images/uploads");
+    },
+
+    //? What should be the name of the file
+        filename : function (req, file, cb){
+            return cb(null, `${Date.now()}-${file.originalname}`);
+        },
+});
+
+const upload = multer({ storage });
 var theDate= new Date().getFullYear();
 
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.urlencoded({extended : false}));
 
 app.use(express.static("public"));
 
@@ -37,13 +51,22 @@ app.post("/submit", (req, res) =>
         res.render("new-blog.ejs",
             {
                 adminName : user,
+                thisYear : theDate,
             }
         )
     }
     
     else{
-        res.render("auth.ejs");
+        res.render("auth.ejs",{
+            thisYear : theDate,
+        });
     }
+})
+
+
+app.post("/upload", upload.single("highlight"), (req, res) =>
+{   
+    return res.redirect("/admin");
 })
 
 app.get("/about", (req, res)=>
